ab38f91333003990d2c4cabc3a4aace266595afd
sujatajachak <50923156+sujatajachak@users.noreply.github.com>
2019-07-23 20:15:17 -0500

    Add files via upload

---
+++ Reports.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class Reports
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim crpt As New TestReport1()
+        crvTest.ReportSource = crpt
+
+    End Sub
+
+End Class
